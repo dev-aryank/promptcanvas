fc385c60f7da9877a7ae324764b630e6b8d8cd1a
Aryxn18 <panchasara.aryan18@gmail.com>
2026-08-17 12:10:12 +0530

    initialize PromptCanvas Spring Boot project

---
+++ src/main/java/dev/aryank/promptcanvas/PromptcanvasApplication.java
@@ -0,0 +1,13 @@
+package dev.aryank.promptcanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptcanvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptcanvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/aryank/promptcanvas/PromptcanvasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aryank.promptcanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptcanvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
